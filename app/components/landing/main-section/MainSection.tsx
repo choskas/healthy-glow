d2b2c106a911d53dac74b87b3eb1f7f0efe3fbc4
choskas <choskasdelta@hotmail.com>
2024-02-23 12:02:06 -0600

    fix: z index

--- app/components/landing/main-section/MainSection.tsx
+++ app/components/landing/main-section/MainSection.tsx
@@ -58,7 +58,7 @@ const MainSection = () => {
           <div className="w-full md:w-3/5 py-6 text-center flex justify-end">
             <img
               alt="logo"
-              className="w-full md:w-4/5 z-50"
+              className="w-full md:w-4/5 z-1"
               src="/imgs/landing/main-image.png"
             />
           </div>
